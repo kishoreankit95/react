27c977b26f5e6f3e2e66147b1732316aaf9563a0
Ankit Kishore <kishore.ankit95@gmail.com>
2024-07-07 08:38:50 +0530

    adding files for storefront

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import './App.scss';
+import React, {useReducer} from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import PhoneBookForm from './phonebook/PhoneBookForm';
 import Converter from './currencyConverter/Converter';
@@ -7,21 +8,41 @@ import Home from './home/index';
 import Box from './tictactoe/Box';
 import Chat from './chatSupport/Chat';
 import Display from './quizGame/Display';
+import StoreList from "./storeFront/StoreList";
+import {reducer} from "./reducers/Reducer";
+
+export const ContextDispatch = React.createContext();
 
 function App() {
+
+  const initialState = {
+    selItem: null,
+    actPrice: null,
+    cName: null,
+    cAddress: null,
+    cEmail: null,
+    cNumber: null,
+    ccNumber: null
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
       
       <div>
         <Navbar />
-        <Routes>
-          <Route path="/react" element={<Home />} />
-          <Route exact path="/phonebook" element={<PhoneBookForm/>} />
-          <Route exact path="/currconv" element={<Converter/>} />
-          <Route exact path="/tictactoe" element={<Box/>} />
-          <Route exact path="/chatsupport" element={<Chat/>} />
-          <Route exact path="/quizgame" element={<Display/>} />
-        </Routes>
+        <ContextDispatch.Provider value={{state: state, stateChanger: dispatch}}>
+          <Routes>
+            <Route path="/react" element={<Home />} />
+            <Route exact path="/phonebook" element={<PhoneBookForm/>} />
+            <Route exact path="/currconv" element={<Converter/>} />
+            <Route exact path="/tictactoe" element={<Box/>} />
+            <Route exact path="/chatsupport" element={<Chat/>} />
+            <Route exact path="/quizgame" element={<Display/>} />
+            <Route exact path="/storefront" element={<StoreList/>} />
+          </Routes>
+        </ContextDispatch.Provider>
       </div>
     </div>
   )
--- src/home/Navbar.js
+++ src/home/Navbar.js
@@ -10,6 +10,7 @@ function Navbar() {
             <div className='link'><Link to="./tictactoe">Tictactoe</Link></div>
             <div className='link'><Link to="./chatsupport">Chat Support</Link></div>
             <div className='link'><Link to="./quizgame">Quiz Game</Link></div>
+            <div className='link'><Link to="./storefront">Store Front</Link></div>
         </div>
     )
 }
